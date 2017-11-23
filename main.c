7a94746f2866364aaf0a0cc23adfd2872dd1895a
marwawer <32923517+marwawer@users.noreply.github.com>
2017-11-23 10:59:00 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+
+void wczytaj (double *a1, double *b1, double *c1, double *a2, double *b2, double *c2){
+
+
+	puts ("Podaj wyznacznik a1"); scanf("%lf", a1);
+	puts ("Podaj wyznacznik b1"); scanf("%lf", b1);
+	puts ("Podaj wyznacznik c1"); scanf("%lf", c1);
+	puts ("Podaj wyznacznik a2"); scanf("%lf", a2);
+	puts ("Podaj wyznacznik b2"); scanf("%lf", b2);
+	puts ("Podaj wyznacznik c2"); scanf("%lf", c2);
+
+}
+
+
+	double wyznacznik_glowny ( double a1, double a2, double b1, double b2){
+	return a1*b2 - a2*b1;
+}
+
+	double wyznacznik_x (double c1, double c2, double b1, double b2){
+	return c1*b2 - c2*b1;
+}
+
+	double wyznacznik_y (double a1, double a2, double c1, double c2){
+	return	a1*c2 - a2*c1;
+}
+int main()
+{
+
+	double a1, b1, c1, a2, b2, c2;
+	double W, Wx, Wy;
+	double x, y;
+	wczytaj (&a1, &b1, &c1, &a2, &b2, &c2);
+	W=wyznacznik_glowny( a1, b1, a2, b2);
+	Wx=wyznacznik_x( c1, c2, b1, b2);
+	Wy=wyznacznik_y( a1, a2, c1, c2);
+	x = Wx / W;
+	y = Wy / W;
+	printf ("x = %lf, y = %lf", x, y);
+
+	return 0;
+}
